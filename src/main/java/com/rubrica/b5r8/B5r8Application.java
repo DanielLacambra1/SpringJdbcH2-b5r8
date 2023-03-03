8dc201b2f02218633b4108f38a78896f90fb344b
DanielLacambra1 <lacambra.sidan21@zaragoza.salesianos.edu>
2023-03-03 12:17:19 +0100

    spring JDBC with H2 b5r8

---
+++ src/main/java/com/rubrica/b5r8/B5r8Application.java
@@ -0,0 +1,13 @@
+package com.rubrica.b5r8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B5r8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B5r8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rubrica/b5r8/B5r8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubrica.b5r8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B5r8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
